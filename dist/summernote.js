31dc428c0d3137219e4e93493e1f09ebd940d174
Stephen Young <stephen.young@axisto.com>
2014-07-18 11:48:52 +0100

    Adding distribution versions of summernote

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013 Alan Hong. and outher contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2014-07-15T15:56Z
+ * Date: 2014-07-18T10:48Z
  */
 (function (factory) {
   /* global define */
@@ -2983,6 +2983,10 @@
 
     var hToolbarAndPopoverClick = function (event) {
       var $btn = $(event.target).closest('[data-event]');
+
+      if ($btn.is('a')) {
+        event.preventDefault();
+      }
 
       if ($btn.length) {
         var sEvent = $btn.attr('data-event'), sValue = $btn.attr('data-value');
